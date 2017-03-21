516dc5b0b4c40523e6bac3800698b1717f9503b0
Jezza1970 <jeremycoathup@gmail.com>
2017-03-21 09:08:29 +0000

    thrid commit

---
+++ public/scripts.js
@@ -0,0 +1,129 @@
+/*
+WHISPERS
+*/
+// declare variables
+var username = document.getElementById("username");
+var connect = document.getElementById("connect");
+var chatLogin = document.getElementById("chatLogin");
+var chatWindow = document.getElementById("chatWindow");
+var message = document.getElementById("message");
+var sendMessage = document.getElementById("sendMessage");
+var userLIst = document.getElementById("userList");
+var socket = io.connect(); //socket must be asked for on both server and client
+
+//event: user sends message
+sendMessage.addEventListener("click", function(e){
+  e.preventDefault();
+  socket.emit('message', message.value);
+  message.value = "";
+});
+//listening for messages sent by server
+socket.on('new message', function(data){
+  if (!data.msg == " "){
+  chatWindow.innerHTML += "<div style='color:"+data.color+"' class='chathandle'>"+data.user+":</div></strong> <div style='background-color:"+data.color+"' id='chatmessage'>"+data.msg+"</div>";
+  if (!data.list == " "){
+    chatWindow.innerHTML += "<div id="+data.user+" class='privateReplyList'>"+data.list+"</div>";
+  }
+  updateScroll();
+}
+});
+//keeps scrollbar at bottom
+function updateScroll(){
+    var element = document.getElementById("chatWindow");
+    element.scrollTop = element.scrollHeight;
+}
+//listening for username details after connection
+connect.addEventListener("click", function(e){
+  e.preventDefault();
+  if (!username.value == " "){
+    // check for duplicate user
+    socket.on('duplicate username', function(data){
+      if(data == true){
+        alert("username taken");
+        return;
+      }
+    });
+    //no duplicate found... continue
+    socket.emit('new user', username.value, function(){
+      chatLogin.innerHTML = "<p>You are connected as: <span id='chatname'>" + username.value + "</span></p>";
+    });
+  }
+});
+//updates online user list
+socket.on('get users', function(data){
+  userList.innerHTML ="<h2>Online:</h2>";
+  for (i=0; i<data.length; i++){
+    userList.innerHTML += "<li id="+data[i]+" class='user'>"+data[i]+"</li>";
+  }
+  userList.innerHTML += "<span id='clear'>Return to global chat</span";
+});
+
+// if user selects a private list then sends details of list to server
+var privateUserList =[];
+document.addEventListener("click", function(e){
+  if(e.target && e.target.className == "user"){
+    if (privateUserList.length == 0){
+      var users = document.getElementsByClassName("user");
+      for (var i=0; i < users.length; i++){
+        users[i].style.color = "white";
+      }
+    }
+    var targetUser = document.getElementById(e.target.id);
+    for(var i=0; i<privateUserList.length; i++) {
+      if (privateUserList[i] == e.target.id){
+        targetUser.style.color = "white";
+        var index = privateUserList.indexOf(e.target.id);
+        if (index > -1) {
+          privateUserList.splice(index, 1);
+          console.log(privateUserList);
+          socket.emit('private message', privateUserList);
+          return;
+        }
+      }
+    }
+  targetUser.style.color = "green";
+  privateUserList.push(e.target.id);
+  console.log(privateUserList);
+  socket.emit('private message', privateUserList);
+  }
+});
+
+document.addEventListener("click", function(e){
+  if(e.target && e.target.className == "privateReplyList"){
+    resetList();
+    var str = e.target.textContent;
+    var privateUserList = str.split(",");
+    var senderReply = e.target.id;
+    privateUserList.push(senderReply);
+    var index = privateUserList.indexOf(username.value);
+    if (index > -1) {
+    privateUserList.splice(index, 1);
+  }
+    var users = document.getElementsByClassName("user");
+    for (var y = 0; y < users.length; y++){
+      for (var z = 0; z < privateUserList.length; z++){
+        if (users[y].textContent == privateUserList[z]){
+
+          users[y].style.color = "green";
+        }
+      }
+    }
+    console.log(privateUserList);
+    //return array = privateUserList;
+    socket.emit('private message', privateUserList);
+    }
+    //clears private user list
+    if (e.target && e.target.id == "clear"){
+      resetList();
+    }
+});
+
+function resetList(){
+  console.log("heh");
+  var list = document.getElementsByClassName('user');
+  for (var x = 0; x < list.length; x++){
+    list[x].style.color = "white";
+  }
+  privateUserList = [];
+  socket.emit('private message', privateUserList);
+}
---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+connections = [];
+users = [];
+
+var privateUserList;
+
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8000, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+console.log('server running...');
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on ('connection', function(socket){
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  //on user disconnections
+  socket.on ('disconnect', function(data){
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  //private message - collects private list usernames
+  socket.on('private message', function(data){
+    socket.privateList = data;
+  });
+
+  //send message
+  socket.on('message', function(data){
+    if(socket.privateList){
+    if (socket.privateList.length > 0 ){
+      for(var j = 0; j < socket.privateList.length; j++){
+        for(var i= 0; i< connections.length; i++){
+          if(socket.privateList[j] == connections[i].username){
+            console.log("send message to:" + connections[i].username);
+            connections[i].emit('new message', {msg:data, user:socket.username, list:socket.privateList, color: socket.userColour});
+          }
+        }
+      }
+      socket.emit('new message', {msg:data, user:socket.username, list:socket.privateList, color: socket.userColour});
+    }
+    else {
+        io.sockets.emit('new message', {msg:data, user:socket.username, color: socket.userColour});
+    }
+  }
+  });
+  //new user
+  socket.on('new user', function(data, callback){
+    for (var a = 0; a < users.length; a++){
+      var duplicate = false;
+      if (data == users[a]){
+        duplicate = true;
+        socket.emit('duplicate username', duplicate);
+        return;
+      }
+    }
+    var colourArray = ["#fff8c6", "#ffff00", "#ffd700", "#b1bb17", "#008000", "#006400", "#82caff", "#0000cd", "#191970", "#ffa500", "#f88017", "#ff7f50", "#ff0000", "#8b0000", "#faafba", "#f660ab", "#ff1493", "#c45aec", "#8b008b", "#800080", "#e2a76f", "#806517", "#8b4513", "#999999", "#666666", "#333333", "#000000"];
+    socket.userColour = colourArray[Math.floor(Math.random() * colourArray.length)]
+    socket.privateList = [];
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('get users', users);
+  }
+});
