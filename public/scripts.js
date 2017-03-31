183358f5157e98feb3f449e9870098004e09eabc
Jezza1970 <jeremycoathup@gmail.com>
2017-03-31 08:49:24 +0100

    event delegators

--- public/scripts.js
+++ public/scripts.js
@@ -10,57 +10,10 @@ var message = document.getElementById("message");
 var sendMessage = document.getElementById("sendMessage");
 var userLIst = document.getElementById("userList");
 var socket = io.connect(); //socket must be asked for on both server and client
-
-//event: user sends message
-sendMessage.addEventListener("click", function(e){
-  e.preventDefault();
-  socket.emit('message', message.value);
-  message.value = "";
-});
-//listening for messages sent by server !!
-socket.on('new message', function(data){
-  if (!data.msg == " "){
-  chatWindow.innerHTML += "<div style='color:"+data.color+"' class='chathandle'>"+data.user+":</div></strong> <div style='background-color:"+data.color+"' id='chatmessage'>"+data.msg+"</div>";
-  if (!data.list == " "){
-    chatWindow.innerHTML += "<div id="+data.user+" class='privateReplyList'>Sshh... click to reply<br>"+data.list+"</div>";
-  }
-  updateScroll();
-}
-});
-//keeps scrollbar at bottom
-function updateScroll(){
-    var element = document.getElementById("chatWindow");
-    element.scrollTop = element.scrollHeight;
-}
-//listening for username details after connection
-connect.addEventListener("click", function(e){
-  e.preventDefault();
-  if (!username.value == " "){
-    // check for duplicate user
-    socket.on('duplicate username', function(data){
-      if(data == true){
-        username.value="";
-        username.placeholder = "Username taken!";
-        return;
-      }
-    });
-    //no duplicate found... continue
-    socket.emit('new user', username.value, function(){
-      chatLogin.innerHTML = "<p>You are connected as: <span id='chatname'>" + username.value + "</span></p>";
-    });
-  }
-});
-//updates online user list
-socket.on('get users', function(data){
-  userList.innerHTML ="<h2>Online:</h2>";
-  for (i=0; i<data.length; i++){
-    userList.innerHTML += "<li id="+data[i]+" class='user'>"+data[i]+"</li>";
-  }
-  userList.innerHTML += "<span id='clear'>Return to global chat</span";
-});
-
-// if user selects a private list then sends details of list to server
 var privateUserList =[];
+
+//set up event delegators
+
 document.addEventListener("click", function(e){
   if(e.target && e.target.className == "user"){
     if (privateUserList.length == 0){
@@ -84,9 +37,7 @@ document.addEventListener("click", function(e){
   //console.log(privateUserList);
   socket.emit('private message', privateUserList);
   }
-});
 
-document.addEventListener("click", function(e){
   if(e.target && e.target.className == "privateReplyList"){
     resetList();
     var str = e.target.textContent;
@@ -114,6 +65,54 @@ document.addEventListener("click", function(e){
     if (e.target && e.target.id == "clear"){
       resetList();
     }
+    if(e.target && e.target.id == "connect"){
+      e.preventDefault();
+      if (!username.value == " "){
+        // check for duplicate user
+        socket.on('duplicate username', function(data){
+          if(data == true){
+            username.value="";
+            username.placeholder = "Username taken!";
+            return;
+          }
+        });
+        //no duplicate found... continue
+        socket.emit('new user', username.value, function(){
+          chatLogin.innerHTML = "<p>You are connected as: <span id='chatname'>" + username.value + "</span></p>";
+        });
+      }
+    }
+});
+
+//event: user sends message
+sendMessage.addEventListener("click", function(e){
+  e.preventDefault();
+  socket.emit('message', message.value);
+  message.value = "";
+});
+//listening for messages sent by server !!
+socket.on('new message', function(data){
+  if (!data.msg == " "){
+  chatWindow.innerHTML += "<div style='color:"+data.color+"' class='chathandle'>"+data.user+":</div></strong> <div style='background-color:"+data.color+"' id='chatmessage'>"+data.msg+"</div>";
+  if (!data.list == " "){
+    chatWindow.innerHTML += "<div id="+data.user+" class='privateReplyList'>Sshh... click to reply<br>"+data.list+"</div>";
+  }
+  updateScroll();
+}
+});
+//keeps scrollbar at bottom
+function updateScroll(){
+    var element = document.getElementById("chatWindow");
+    element.scrollTop = element.scrollHeight;
+}
+
+//updates online user list
+socket.on('get users', function(data){
+  userList.innerHTML ="<h2>Online:</h2>";
+  for (i=0; i<data.length; i++){
+    userList.innerHTML += "<li id="+data[i]+" class='user'>"+data[i]+"</li>";
+  }
+  userList.innerHTML += "<span id='clear'>Return to global chat</span";
 });
 
 function resetList(){
